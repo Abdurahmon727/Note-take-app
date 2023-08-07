203737ff0f22506b5fdfad1a82ea863c562b201c
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2023-08-07 19:22:43 +0500

    fix: uncomment try catch

--- lib/features/home/data/repositories/home_repo.dart
+++ lib/features/home/data/repositories/home_repo.dart
@@ -8,12 +8,12 @@ import '../models/event_model.dart';
 class HomeRepositoryImpl implements HomeRepository {
   @override
   Future<Either<StorageFailure, void>> addAnEvent(EventModel model) async {
-    // try {
-    await sl<HomeLocalDataSource>().addModel(model);
-    return Right(null);
-    // } catch (e) {
-    //   return Left(StorageFailure(errorMessage: 'Unable to add'));
-    // }
+    try {
+      await sl<HomeLocalDataSource>().addModel(model);
+      return Right(null);
+    } catch (e) {
+      return Left(StorageFailure(errorMessage: 'Unable to add'));
+    }
   }
 
   @override
@@ -51,11 +51,11 @@ class HomeRepositoryImpl implements HomeRepository {
 
   @override
   Future<Either<StorageFailure, void>> editAnEvent(EventModel model) async {
-    // try {
-    await sl<HomeLocalDataSource>().editAnEvent(model);
-    return Right(null);
-    // } catch (e) {
-    //   return Left(StorageFailure(errorMessage: 'Something went wrong'));
-    // }
+    try {
+      await sl<HomeLocalDataSource>().editAnEvent(model);
+      return Right(null);
+    } catch (e) {
+      return Left(StorageFailure(errorMessage: 'Something went wrong'));
+    }
   }
 }
